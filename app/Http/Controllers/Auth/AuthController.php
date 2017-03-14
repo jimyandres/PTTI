893db14a97be44bda69a2b8240a84688d37cb8e4
JohanQuiroga <quirogacj@utp.edu.co>
2017-03-13 19:52:36 -0500

    Function that was modified in laravel sources, now its defined on AuthController to override source

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\Grupo;
+use App\Models\Institucion;
 use App\Models\Solicitude;
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
@@ -83,4 +88,127 @@ class AuthController extends Controller
             'tipoUsuario_codigoTipoUsuario' => $data['tipoUsuario_codigoTipoUsuario'],
         ]);
     }
+
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->loginUsername() => 'required', 'password' => 'required',
+        ]);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        $throttles = $this->isUsingThrottlesLoginsTrait();
+
+        if ($throttles && $this->hasTooManyLoginAttempts($request)) {
+            return $this->sendLockoutResponse($request);
+        }
+
+        $credentials = $this->getCredentials($request);
+
+        /*if (Auth::attempt($credentials, $request->has('remember'))) {
+            return $this->handleUserWasAuthenticated($request, $throttles);
+        }*/
+
+        if (Auth::validate($credentials)) {
+            $user = Auth::getLastAttempted();
+            if ($user->active) {
+                Auth::login($user, $request->has('remember'));
+                return redirect()->intended($this->redirectPath());
+            } else {
+                return redirect($this->loginPath()) // Change this to redirect elsewhere
+                ->withInput($request->only('email', 'remember'))
+                    ->withErrors([
+                        'active' => 'Debes estar activo para iniciar sesión'
+                    ]);
+            }
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        if ($throttles) {
+            $this->incrementLoginAttempts($request);
+        }
+
+        return redirect($this->loginPath())
+            ->withInput($request->only($this->loginUsername(), 'remember'))
+            ->withErrors([
+                $this->loginUsername() => $this->getFailedLoginMessage(),
+            ]);
+    }
+
+    public function getRegister()
+    {
+        $institucion = Institucion::lists('nombre', 'codigoInstitucion');
+        /*$default = ['Seleccionar una' => ''];
+        $array = (array)$institucion;
+        array_unshift($array, $default);
+        $institucion->exchangeArray($array);*/
+        return view('auth.register', compact('institucion'));
+    }
+
+
+    public function getGrupos(Request $request, $codigoInstitucion)
+    {
+        if ($request->ajax()) {
+            $grupos = Grupo::grupos($codigoInstitucion);
+            return response()->json($grupos);
+        }
+    }
+
+    public function postRegister(Request $request)
+    {
+        //print_r($request->all());
+        $validator = $this->validator($request->all());
+
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+
+        if (!User::where('id', '=', $request->id)->get()->isEmpty()) {
+            $usuario = User::find($request->id);
+            if ($usuario->active == 0) {
+                if ($usuario->id != $request->id) {
+                    $validar_id = Validator::make($request->all(), [
+                        'id' => [ 'unique:users', 'unique:solicitudes' ],
+                    ]);
+                    if ($validar_id->fails()) {
+                        return Redirect::to('/auth/register')->withErrors($validar_id);
+                    }
+                }
+                if ($usuario->email != $request->email) {
+                    $validar_email = Validator::make($request->all(), [
+                        'email' => [ 'unique:users', 'unique:solicitudes' ],
+                    ]);
+                    if ($validar_email->fails()) {
+                        return Redirect::to('/auth/register')->withErrors($validar_email);
+                    }
+                }
+                $data = $request->all();
+                $usuario->fill($data);
+                $usuario->active = 1;
+                $usuario->save();
+
+                return Redirect::to('/auth/login')->with('modal_message_success', 'Usuario creado satisfactoriamente');
+            }
+            else {
+                return Redirect::to('/auth/register')
+                    ->with('modal_message_error', 'Este usuario ya existe');
+            }
+        }
+        else {
+            $data = $request->all();
+            if ($request->grupo_codigoGrupo == "") {
+                $data['grupo_codigoGrupo'] = null;
+            }
+
+            Auth::login($this->create($data));
+
+            return Redirect::to($this->redirectPath())->with('modal_message_success', 'Se ha enviado la solicitud satisfactoriamente');
+        }
+    }
+
 }
